31abd56902cea4f16956f20580325a5a4ff3b25b
Andrew Hill <andrewwestbrookhill@utexas.edu>
2020-07-06 23:18:48 -0500

    Added loading to exercise selector

--- Quick Fitness/ExerciseSelector.swift
+++ Quick Fitness/ExerciseSelector.swift
@@ -9,23 +9,59 @@
 
 import UIKit
 
-class ExerciseSelector: UIViewController {
+let exerciseSelectCellID: String = "ExerciseSelectCell"
+let newExerciseCellID: String = "NewExerciseCell"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+class ExerciseSelector: UIViewController, UITableViewDataSource, UITableViewDelegate {
+	@IBOutlet weak var tableView: UITableView!
+	
+	var exercises: [Exercise] = CoreDataManager.fetchAllExercises()
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		tableView.delegate = self
+		tableView.dataSource = self
+	}
+	
+	func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+		return exercises.count + 1
+	}
+	
+	func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        // Do any additional setup after loading the view.
-    }
-    
+		// Configure the cell...
+		let row = indexPath.row
+		
+		if row == exercises.count {
+			return loadNewExerciseCell(tableView, cellForRowAt: indexPath)
+		} else {
+			return loadExerciseCell(tableView, cellForRowAt: indexPath)
+		}
+	}
+	
+	func loadNewExerciseCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: newExerciseCellID, for: indexPath)
+		cell.textLabel?.text = "Add Exercise"
+		return cell
+	}
+	
+	func loadExerciseCell(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: exerciseSelectCellID, for: indexPath)
+		let exercise = exercises[indexPath.row]
+		cell.textLabel?.text = exercise.name
+		return cell
+	}
+	
+	// TODO: Add option to delete cell
 
-    /*
-    // MARK: - Navigation
+	/*
+	// MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+		// Get the new view controller using segue.destination.
+		// Pass the selected object to the new view controller.
+	}
+	*/
 
 }
